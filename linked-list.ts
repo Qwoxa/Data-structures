6a67e73e5d36b167f49ba8adc25cafe329fbc24c
Nick <nick@rhinodispatch.com>
2019-11-05 04:13:23 -0500

    pop() method

--- linked-list.ts
+++ linked-list.ts
@@ -1,31 +1,32 @@
 export interface INode<E> {
-  next: INode<E>;
+  next: INode<E> | null;
   data: E;
 }
 
 export interface ILinkedList<E> {
-
+  unshift(nodeValue: E): number;
+  shift(): E | null;
+  push(nodeValue: E): number;
+  pop(): E | null;
 }
 
 class Node<E> implements INode<E> {
-  public next: INode<E>;
-  public data: E
+  public next: INode<E> | null;
+  public data: E;
 
   constructor(data: E) {
     this.data = data;
     this.next = null;
   }
-  
 }
 
 export class LinkedList<E> implements ILinkedList<E> {
-  private head: Node<E>;
-  private tail: Node<E>;
+  private head: Node<E> | null;
+  private tail: Node<E> | null;
   private currentSize: number;
 
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.head = this.tail = null;
     this.currentSize = 0;
   }
   
@@ -57,17 +58,18 @@ export class LinkedList<E> implements ILinkedList<E> {
    * Method shift removes the first element from a list
    * @return The value of the deleted Node
    */
-  public shift(): E | void {
+  public shift(): E | null {
     // If the head is null, there is nothing to shift
     if (this.head === null) {
       return null;
     }
 
     // Save the node. Assign the second Node to be the head
-    // If the list consisted only from one node
-    // then set the tail to be null
     const deletedNode = this.head;
     this.head = this.head.next;
+
+    // If the list consisted only from one node
+    // then set the tail to be null
     if (this.head === null) this.tail = null;
     this.currentSize--;
 
@@ -104,7 +106,52 @@ export class LinkedList<E> implements ILinkedList<E> {
    * Removes the last Node from a list
    * @return The value of the deleted Node
    */
-  // public pop(): E {
-    
-  // }
+  public pop(): E | null {
+    // If the head is null, there is nothing to pop
+    if (this.head === null) {
+      return null;
+    }
+
+    // If the list contains only one node
+    // return the executing of shift method
+    if (this.head === this.tail) {
+      return this.shift();
+    }
+
+    // Get the one before the last node
+    let beforeLast = this.head;
+    while(beforeLast.next !== this.tail) {
+      beforeLast = beforeLast.next
+    }
+
+    // Extract data, set the next to be null
+    // set new value to tail
+    const { data } = beforeLast.next;
+    beforeLast.next = null;
+    this.tail = beforeLast;
+
+    this.currentSize--;
+    return data;
+  }
+
+  get Tail() {
+    return this.tail;
+  }
+
+  get Head() {
+    return this.head;
+  }
 }
+
+const list = new LinkedList<string>();
+
+list.push('Ann');
+list.push('Craig');
+
+
+console.log(list.pop());
+console.log(list.pop());
+
+
+console.log(list.Head)
+console.log(list.Tail)
