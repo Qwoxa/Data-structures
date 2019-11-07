47a45b5251c8d861b809acc3875f2fa41c1bf2f5
Nick Dyson <qwoxa1@gmail.com>
2019-11-07 00:59:55 -0500

    slight modification in hash fn

--- sandbox.ts
+++ sandbox.ts
@@ -1,7 +1,9 @@
 const hash = (key: string): number => {
+  const g = 31;
+
   return Array.from(key)
   .map(char => char.charCodeAt(0))
-  .reduce((sum, cur) => sum + cur, 0);
+  .reduce((sum, cur) => g * sum + cur, 0);
 };
 
-console.log(hash(''));
+console.log(hash('qwdsf'));
