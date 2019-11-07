8c5963f95e010e613a4a555a8b9e1740e296a0dd
Nick Dyson <qwoxa1@gmail.com>
2019-11-07 01:32:00 -0500

    tablesize

--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,14 @@
 const hash = (key: string): number => {
   const g = 31;
 
-  const hash = Array.from(key)
-  .map(char => char.charCodeAt(0))
-  .reduce((sum, cur) => g * sum + cur, 0);
+  let hash = Array.from(key)
+    .map(char => char.charCodeAt(0))
+    .reduce((sum, cur) => g * sum + cur, 0);
 
-  return hash > 0 ? hash : ~hash;
+  // make sure it is positive int
+  hash = hash > 0 ? hash : ~hash;
+
+  return hash; // % tablesize
 };
 
 console.log(hash('Nick'));
